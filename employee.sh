603f3d7539fe1ab4be7da2453fe97343a9a77e39
Kumarivarsha-14 <varshakumari2714@gmail.com>
2023-03-01 18:35:39 -0800

    [varsha]Add::shell script pgm of employee wage using switch case

--- employee.sh
+++ employee.sh
@@ -1,32 +1,31 @@
 
-#Calculating wage for part time Employee.
+# Solving using Switch Case statement 
 
 
 #!/bin/bash
 
-#variable
+#variables
 random_check=$(( RANDOM % 3 )) # 0/1/2
 
 is_present_full_time=1
 is_present_part_time=2
 wage_per_hour=20
-full_time_hour=8
-part_time_hour=4
-salary=0
 
 #selection
-if [ $random_check -eq $is_present_full_time ]
-then
-salary=$(( $full_time_hour * $wage_per_hour ))
-echo "Employee is present for full time"
-echo "Salary of the full time employee is $salary"
+case $random_check in
 
-elif [ $random_check -eq $is_present_part_time ]
-then
-salary=$(( $part_time_hour * $wage_per_hour ))
-echo "Employee is present for part time"
-echo "Salary of part time employee is $salary"
+$is_present_full_time) emp_hours=8
+echo " Employee is present for full time"
+;;
 
-else
-echo "Employee is absent and salary is $salary"
-fi
+$is_present_part_time) emp_hours=4
+echo " Employee is present for part time"
+;;
+*) emp_hours=0
+echo " Employee is absent"
+;;
+esac
+
+salary=$(( $emp_hours * $wage_per_hour))
+echo " Salary of an employee is $salary "
+
